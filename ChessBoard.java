fe02acab33b9ec49dc7fe59ab6c1747a4cc151b3
JasonOoi20 <46021817+JasonOoi20@users.noreply.github.com>
2020-08-20 14:34:15 +0800

    Add files via upload
    First initial version

---
+++ ChessBoard.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class ChessBoard extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Webale Chess Assignment Beta");
+        GridPane gridPane = new GridPane();
+        for (int row = 0; row < 8 ; row++){
+            for (int col = 0; col < 7; col++){
+                Button button = new Button();
+                button.setMinHeight(100.0);
+                button.setMinWidth(100.0);
+                gridPane.add(button, col, row);
+            }
+        }
+        Scene scene = new Scene(gridPane, 700,800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
