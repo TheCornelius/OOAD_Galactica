285d79549c159196632b596d16d459adf156c6e0
JasonOoi20 <46021817+JasonOoi20@users.noreply.github.com>
2020-08-21 12:19:14 +0800

    Jason: Added chess pieces resources and btn style.

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,30 +1,43 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.awt.*;
+import java.util.ArrayList;
+
 
 public class ChessBoard extends Application{
-
+    private ArrayList<Button> buttons;
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Webale Chess Assignment Beta");
         GridPane gridPane = new GridPane();
+        buttons = new ArrayList<Button>();
+        int btnCount = 0;
         for (int row = 0; row < 8 ; row++){
             for (int col = 0; col < 7; col++){
-                Button button = new Button();
-                button.setMinHeight(100.0);
-                button.setMinWidth(100.0);
-                gridPane.add(button, col, row);
+                buttons.add(new Button());
+                buttons.get(btnCount).setId(String.valueOf(btnCount));
+                buttons.get(btnCount).setMinHeight(100.0);
+                buttons.get(btnCount).setMinWidth(100.0);
+                buttons.get(btnCount).setStyle("-fx-background-color: #FFFFFF;-fx-border-color: rgb(50,87,138);-fx-border-width: 2;");
+                gridPane.add(buttons.get(btnCount), col, row);
+                System.out.println(buttons.get(btnCount).getId());
+                btnCount++;
             }
         }
+        /*
+        Image image = new Image(getClass().getResourceAsStream("red_arrow.png"));
+        ImageView imageView = new ImageView(image);
+        button.setGraphic(imageView);
+        */
+
         Scene scene = new Scene(gridPane, 700,800);
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        System.out.println("Hello World");
     }
 
     public static void main(String[] args) {
